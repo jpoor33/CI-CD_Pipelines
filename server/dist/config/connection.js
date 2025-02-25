de3970e2423908816436ed28b966adb3a05fae70
jpoor33 <74677747+jpoor33@users.noreply.github.com>
2025-02-25 14:30:41 -0500

    pushing changes

--- server/dist/config/connection.js
+++ server/dist/config/connection.js
@@ -1,14 +1,9 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
 dotenv.config();
-
-
 const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
-
 mongoose
-  .connect(mongoURI)
-  .then(() => console.log('✅ MongoDB connected successfully'))
-  .catch((err) => console.error('❌ MongoDB connection error:', err));
-
+    .connect(mongoURI)
+    .then(() => console.log('✅ MongoDB connected successfully'))
+    .catch((err) => console.error('❌ MongoDB connection error:', err));
 export default mongoose.connection;
